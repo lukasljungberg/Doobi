a831f3c10e9a8071a58959b340ebd631638b20c8
lukasljungberg <lukas-ljungberg@outlook.com>
2025-01-03 21:37:56 +0100

    commit

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify, send_file, Response
 import os
-
+import json
 # Initialize Flask and pygame mixer
 app = Flask(__name__)
 
-# Global variables
-songs = []
-current_song_index = -1
-
 @app.route('/')
 def index():
-    return render_template('index.html', songs=songs)
+    return render_template('index.html')
 
 @app.route('/add_songs', methods=['POST'])
 def add_songs():
-    global songs
-    uploaded_files = request.files.getlist("files")
-    for file in uploaded_files:
-        file_path = os.path.join("uploads", file.filename)
-        file.save(file_path)
-        songs.append(file_path)
-    return jsonify({"status": "success", "songs": [os.path.basename(song) for song in songs]})
+    file = request.files.getlist("song")[0]
+    cover = request.files.getlist("cover")[0]
+    artist = request.form.get("artist")
+    genre = request.form.get("genre")
+    obj = {}
+    file_path = os.path.join("uploads/songs", file.filename)
+    file.save(file_path)
+    obj['file_path'] = file_path
+    obj['artist'] = artist
+    obj['genre'] = genre
+    cover_path = os.path.join("uploads/covers", cover.filename)
+    cover.save(cover_path)
+    obj['cover_path'] = cover_path
+    with open('uploads/objects/'+file.filename+artist+'.json', 'w') as fp:
+        json.dump(obj, fp)
+    return Response(200)
 
-@app.route('/uploads/<string:song>', methods=['GET'])
+@app.route('/uploads/songs/<string:song>', methods=['GET'])
 def get_song(song):
-    dir = os.listdir('./uploads')
+    dir = os.listdir('./uploads/songs')
     idx = int(song)
     try:
         dir[int(song)]
     except:
         idx = (idx + 1) % (len(dir))
 
-    return send_file('uploads/'+dir[idx])
+    return send_file('uploads/songs/'+dir[idx])
 
 
 if __name__ == "__main__":
