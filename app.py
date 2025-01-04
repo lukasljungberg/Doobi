15d3a0c3eb3269ea67e1f5d7768991b9791cba6c
lukasljungberg <lukas-ljungberg@outlook.com>
2025-01-04 19:28:36 +0100

    commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, jsonify, send_file, Response
+from flask import Flask, render_template, request, jsonify, send_file, Response, stream_with_context
 import os
 import json
+from flask_cors import CORS
 # Initialize Flask and pygame mixer
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -10,33 +12,56 @@ def index():
 
 @app.route('/add_songs', methods=['POST'])
 def add_songs():
-    file = request.files.getlist("song")[0]
-    cover = request.files.getlist("cover")[0]
+    file = request.files.get("song")
+    cover = request.files.get("cover")
     artist = request.form.get("artist")
     genre = request.form.get("genre")
     obj = {}
     file_path = os.path.join("uploads/songs", file.filename)
     file.save(file_path)
-    obj['file_path'] = file_path
+    obj['file_path'] = str(file_path).replace('/', '.')
     obj['artist'] = artist
     obj['genre'] = genre
     cover_path = os.path.join("uploads/covers", cover.filename)
     cover.save(cover_path)
-    obj['cover_path'] = cover_path
+    obj['cover_path'] = str(cover_path).replace('/', '.')
     with open('uploads/objects/'+file.filename+artist+'.json', 'w') as fp:
         json.dump(obj, fp)
-    return Response(200)
+    return jsonify({"status": "success!"})
 
-@app.route('/uploads/songs/<string:song>', methods=['GET'])
-def get_song(song):
-    dir = os.listdir('./uploads/songs')
-    idx = int(song)
+@app.route('/objects/songs/<string:id>', methods=['GET'])
+def download_multiple_files(id):
+    dir = os.listdir('uploads/objects')
+    idx = int(id)
     try:
-        dir[int(song)]
+        dir[idx]
     except:
-        idx = (idx + 1) % (len(dir))
+        if len(dir) > 0:
+            idx = (idx + 1) % (len(dir))
+            with open('uploads/objects/'+dir[idx], 'r') as f:
+                obj = json.load(f)
+            return jsonify(obj)
+        return Response(status=404)
 
-    return send_file('uploads/songs/'+dir[idx])
+# Stream an image file
+@app.route('/stream-image/<string:fp>', methods=['GET'])
+def stream_image(fp):
+    fp = str(fp).replace('.', '/').replace('/jpg', '.jpg').replace('/png', '.png')
+    def generate():
+        with open(fp, 'rb') as f:
+            while chunk := f.read(4096):  # Stream in chunks of 4096 bytes
+                yield chunk
+    return Response(generate(), content_type='image/jpeg')
+
+# Stream an audio file
+@app.route('/stream-audio/<string:fp>', methods=['GET'])
+def stream_audio(fp):
+    fp = str(fp).replace('.', '/').replace('/mp3', '.mp3')
+    def generate():
+        with open(fp, 'rb') as f:
+            while chunk := f.read(4096):  # Stream in chunks of 4096 bytes
+                yield chunk
+    return Response(generate(), content_type='audio/mpeg')
 
 
 if __name__ == "__main__":
